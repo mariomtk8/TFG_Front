58f15a77c66f5b3038bfae597824b4f841a8f4a3
DESKTOP-SGSURQH\Practicas25 <a26931@svalero.com>
2024-09-25 10:33:47 +0200

    pag login

--- TFG_Front2.0/src/router/index.ts
+++ TFG_Front2.0/src/router/index.ts
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Index from '../views/index.vue';
 import RecetasCat from '../views/RecetasCat.vue';
 import Recetas from '../views/Recetas.vue';
+import Login from '../views/login.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'index', component: Index },
     { path: '/RecetasCat/:idCategoria', name: 'RecetasCat', component: RecetasCat },
-    { path: '/Recetas/:id', name: 'Recetas', component: Recetas}
+    { path: '/Recetas/:id', name: 'Recetas', component: Recetas},
+    { path: '/Login', name: 'Login', component: Login}
   ],
 });
 
